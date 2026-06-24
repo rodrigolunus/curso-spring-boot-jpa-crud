5cb04f31ce4bc2290e0114554f980c92f0053fa7
TCE\rodrigo.leitao <rodrigo.leitao@tce.ce.gov.br>
2026-06-24 13:07:15 -0300

    Primeiro commit

---
+++ src/main/java/org/rodrigolunus/codexhyrule/CodexHyruleApplication.java
@@ -0,0 +1,13 @@
+package org.rodrigolunus.codexhyrule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexHyruleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexHyruleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rodrigolunus/codexhyrule/CodexHyruleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rodrigolunus.codexhyrule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexHyruleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
